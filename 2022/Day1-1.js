957a275573c1dd3378c27e4facd944905edc1b71
smcdev <smcdev@outlook.com>
2022-12-03 23:05:30 -0600

    Update Day1-1.js

--- 2022/Day1-1.js
+++ 2022/Day1-1.js
@@ -1,5 +1,23 @@
-let elf1 = [1000, 2000, 3000];
-let elf2 = [4000];
-let elf3 = [5000, 6000];
-let elf4 = [7000, 8000, 9000];
-let elf5 = [10000];
+const readFileSync = require("fs").readFileSync;
+var input = readFileSync("./Day1-1.txt", {
+  encoding: "utf8",
+  flag: "r",
+}).trim();
+
+let splitInput = input.split("\n");
+
+function CalorieCounter(splitInput) {
+  let sum = 0;
+  let greatestNumber = 0;
+  for (let i = 0; i < splitInput.length; ++i) {
+    if (Boolean(splitInput[i]) === true) {
+      sum += splitInput[i] * 1;
+    } else if (Boolean(splitInput[i]) === false) {
+      greatestNumber = Math.max(sum, greatestNumber);
+      sum = 0;
+    }
+  }
+  return [sum, greatestNumber];
+}
+
+console.log(CalorieCounter(splitInput));
